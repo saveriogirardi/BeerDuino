a3ff6c1b97ace900017ba278e0b326141e45060b
SaveGir <user@adm>
2012-07-14 13:53:46 +0200

    Added temperature sensor code, header

---
+++ TemperatureSens.ino
@@ -0,0 +1,13 @@
+/*
+This program identifies and read the temperature from a DS18B20 temperature sensor.
+
+You can find a cheap and preinterfaced one at 
+http://www.dealextreme.com/p/ds18b20-digital-temperature-sensor-module-for-arduino-55-125-c-135047?item=8
+
+created 14 Lug 2012
+by Saverio Girardi
+www.opensourceideas.it
+
+This example code is in the public domain under GNU public license.
+
+*/
